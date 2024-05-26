62e72d61565e53e063c362268ff5edbc5f13de7e
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-05-26 08:26:59 +0300

    Connect To the DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is Running on Port ${port}`)
-})
+  console.log(`Server is Running on Port ${port}`);
+});
