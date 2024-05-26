703f546b6fdb449fe800c90e98e3621fac1c33d6
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-05-26 09:44:09 +0300

    create User Model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URL)
