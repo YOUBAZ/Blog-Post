f669837f4784157ee3be60e91f45ff31f81c24de
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-05-28 16:50:02 +0300

    Create the Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./Pages/SignUp.jsx";
 import Dashboard from "./Pages/Dashboard.jsx";
 import Projects from "./Pages/Projects";
 import Header from "./components/Header.jsx";
+import FooterCom from "./components/Footer.jsx";
 const App = () => {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <FooterCom />
     </BrowserRouter>
   );
 };
