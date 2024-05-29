7999eec8d377abd54da8c4d39a680331c9d66218
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-05-29 14:14:42 +0300

    add Redux Toolkit

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -2,31 +2,37 @@ import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
   try {
-    const { username, email, password } = req.body;
-    if (
-      !username ||
-      !email ||
-      !password ||
-      username === "" ||
-      email === "" ||
-      password === ""
-    ) {
-      next(errorHandler(400, "All Fields are required"));
-    }
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
     await newUser.save();
-    res.json({ message: "Signup Successfully" });
+    res.json("Signup successful");
   } catch (error) {
     next(error);
   }
 };
+
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signin, signup } from "../controllers/authController.js";
 const router = express.Router();
-router.route("/signup").post(signup);
-router.route("/signin").post(signin);
+router.post("/signup", signup);
+router.post("/signin", signin);
 export default router;
