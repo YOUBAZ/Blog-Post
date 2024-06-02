fda405a3c292dacbe17513c0a5c0041dc39f463e
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-06-02 21:19:15 +0300

    Complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -17,11 +17,17 @@ const DashSidebar = () => {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-        <Link to='/dashboard?tab=profile'>
-          <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark">
-            Profile
-          </Sidebar.Item>
-        </Link>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
           <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
