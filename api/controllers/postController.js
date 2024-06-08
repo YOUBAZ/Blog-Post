269a5804ba951b65c6e02f3e04b66641b0066251
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-06-08 18:54:09 +0300

    add show more functionality to the posts result of the dashboard

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -4,7 +4,8 @@ import { errorHandler } from "../utils/error.js";
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "you are not allowed to create a post"));
-  } else if (!req.body.title || !req.body.content) {
+  }
+  if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, "Please provide all required fields"));
   }
   const slug = req.body.title
