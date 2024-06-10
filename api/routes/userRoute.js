d0b6bc8f88d345a21a9d6ad12b8a6934c1f09177
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-06-10 20:09:23 +0300

    add delete user functionality to the admin user

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { test, updateUser, deleteUser, signout, getUsers } from "../controllers/userController.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  signout,
+  getUsers,
+} from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post('/signout', signout)
-router.get('/getusers', verifyToken, getUsers)
+router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 export default router;
