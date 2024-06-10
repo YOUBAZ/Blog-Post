feadc0816a18fbc7541f8ae4a44afeee72afb20b
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-06-10 19:57:23 +0300

    show users to the admin dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar.jsx";
 import DashProfile from "../components/DashProfile.jsx";
 import DashPosts from "../components/DashPosts.jsx";
+import DashUsers from "../components/DashUsers.jsx";
 const Dashboard = () => {
   const location = useLocation();
   const [tab, setTab] = useState("");
@@ -10,16 +11,17 @@ const Dashboard = () => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
     if (tabFromUrl) {
-      setTab(tabFromUrl)
+      setTab(tabFromUrl);
     }
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
-      <div className='mid:w-56'>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="mid:w-56">
         <DashSidebar />
       </div>
       {tab === "profile" && <DashProfile />}
-      {tab === "posts" && <DashPosts/>}
+      {tab === "posts" && <DashPosts />}
+      {tab === "users" && <DashUsers />}
     </div>
   );
 };
