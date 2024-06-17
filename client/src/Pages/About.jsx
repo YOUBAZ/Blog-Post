f2d2f2d07bb486f484bc80a0ff443ab605e78634
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-06-17 19:47:02 +0300

    complete about and projects pages

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,10 +1,22 @@
-
 const About = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Yousef's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Yousef's Blog, a place where I share my thoughts,
+              experiences, and newest updates about full stack programming
+              languages and tools.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,10 +1,14 @@
-
-const Projects = () => {
+import CallToAction from "../components/CallToAction.jsx";
+export default function Projects() {
   return (
-    <div>
-      
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS, and
+        JavaScript! as well as all of the newest and advanced libraries like
+        Next.js , React.js and Node.js
+      </p>
+      <CallToAction />
     </div>
-  )
+  );
 }
-
-export default Projects
