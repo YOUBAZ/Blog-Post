5af06ebc8251104d4e28153731edfd43d249f002
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2025-08-17 05:56:27 +0300

    Pre-deployment setup

--- api/index.js
+++ api/index.js
@@ -7,9 +7,18 @@ import postRoutes from "./routes/postRoute.js";
 import commentRoutes from "./routes/commentRoute.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 const port = 3000;
 dotenv.config();
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow only requests from this origin
+  methods: "GET,POST", // Allow only these methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow only these headers
+};
+
+// Use CORS middleware with specified options
+app.use(cors(corsOptions));
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log("MongoDB Connected Successfully"))
